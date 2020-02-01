589e4fae52d333afc7364b7a7232c53ba4f6ba00
Henrik trehjørningen <Henrik@Henriks-MacBook-Air.local>
2020-02-01 14:57:46 +0100

    Anchor script

---
+++ Assets/Scripts/pendulum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pendulum : MonoBehaviour
+{
+  public GameObject boat;
+  public GameObject hook;
+  public float force = 0;
+  LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+      lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(1, hook.transform.position);
+        hook.GetComponent<Rigidbody>().AddForce(force,0,0);
+    }
+}
